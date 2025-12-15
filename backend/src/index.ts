1fc49bd3c80dfed5efda841c2621d3c894be4f05
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-15 23:34:50 +0545

    add complete index file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -45,3 +45,33 @@ app.use(helmet());
 
 // import and apply rate limiter middleware
 app.use(apiRateLimiter);
+
+// basic route
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World!' });
+});
+
+// import routes
+import authRoutes from './routes/auth.route';
+app.use('/api/auth', authRoutes);
+
+// connect to database
+const port = process.env.PORT || 8000;
+connectDB().then(() => {
+  console.log('Database connected, starting server...');
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
+});
+
+// disconnect from database on app termination
+const handleServerDown = async () => {
+  try {
+    await disconnectDB();
+    console.log('SERVER TERMINATING...');
+    process.exit(0);
+  } catch (error) {
+    console.error('Error during disconnection:', error);
+  }
+};
+
+process.on('SIGINT', handleServerDown); // For Ctrl+C event
+process.on('SIGTERM', handleServerDown); // For termination event
